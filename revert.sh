c3fbbcbf38bbb2b20c381d31776781726209121d
msx98 <me@glub.xyz>
2023-07-21 17:09:03 +0300

    better patch scripts

--- revert.sh
+++ revert.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-cd system/netd
-git reset --hard HEAD~1
-cd ../../
-cd hardware/libhardware
-echo "skipping hardware/libhardware"
-cd ../../
-cd packages/modules/adb
-git reset --hard HEAD~1
-cd ../../../
-cd system/bpf
-git reset --hard HEAD~1
-cd ../
-cd security
-echo "skipping system/security"
-cd ../../
-cd frameworks/base
-echo "skipping frameworks/base"
-cd ../../
+PATCHES_DIR=$1
+REPO_DIR=$2
 
+for dir in $PATCHES_DIR/*/
+do
+	patch_path=`basename $dir`
+	new_path=`printf $patch_path | sed 's/_/\//g'`
+	dest_path="$REPO_DIR/$new_path"
+	echo "Patching in $dest_path"
+	cd $dest_path; git reset --hard HEAD~1
+done
